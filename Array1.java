dd551bb98f7b48852277306d958670ee91939ac5
Tripta10 <triptagagneja@gmail.com>
2021-11-01 14:21:37 +0530

    Array1

---
+++ Array1.java
@@ -0,0 +1,11 @@
+public class Array1{
+    public static void main(String[] args){
+        int marks[] = new int[3];
+        marks[0] = 99;
+        marks[1] = 98;
+        marks[2] = 100;
+        System.out.println(marks[0]);
+
+
+    }
+}
